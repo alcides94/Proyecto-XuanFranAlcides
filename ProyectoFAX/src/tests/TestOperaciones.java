d36e93d9e5cb1d0966951e52cc302ed44230494e
alcides94 <65246102+alcides94@users.noreply.github.com>
2024-04-30 17:56:05 +0200

    Se sube el test de cancelar cuenta

--- ProyectoFAX/src/tests/TestOperaciones.java
+++ ProyectoFAX/src/tests/TestOperaciones.java
@@ -4,11 +4,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import Modelo.Cuenta;
+import util.Operaciones;
+
 class TestOperaciones {
 
+	
+	
+	
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testCancelar() {
+		boolean cuentaEsperada=true;
+		boolean cuentaObtenida=Operaciones.cancelarCuenta(new Cuenta(23,"Xuan",34,true));
+		assertEquals(cuentaEsperada, cuentaObtenida);	
 	}
 
 }
