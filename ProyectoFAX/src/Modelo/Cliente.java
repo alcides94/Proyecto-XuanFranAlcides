d8537865e1f58716e19521caed2261cf993a5b6f
wugena <162334776+wugena@users.noreply.github.com>
2024-04-30 17:15:59 +0200

    crar paquete modelo con clase clientes y cuenta  -Xuan

--- ProyectoFAX/src/Modelo/Cliente.java
+++ ProyectoFAX/src/Modelo/Cliente.java
@@ -1,5 +1,24 @@
 package Modelo;
 
 public class Cliente {
-
+	/**
+	 * Creamos tres variable nombre apellidos y edad
+	 */
+	private String nombre;
+	private String apellidos;
+	private int edad;
+	/**
+	 * 
+	 * @param nombre
+	 * @param apellidos
+	 * @param edad
+	 */
+	public Cliente(String nombre, String apellidos, int edad) {
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.edad = edad;
+	}
+	
+	
+	
 }
