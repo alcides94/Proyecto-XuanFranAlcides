70b725d765f5d24a73f5a673b9f93206b38790a9
FranCiscoBM12 <162335182+FranCiscoBM12@users.noreply.github.com>
2024-04-30 17:46:02 +0200

    Cerra la bocota

--- ProyectoFAX/src/app/Main.java
+++ ProyectoFAX/src/app/Main.java
@@ -1,10 +1,23 @@
 package app;
 
+
+
+
+import Modelo.Cliente;
+import Modelo.Cuenta;
+import util.Operaciones;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Cliente C1=new Cliente ("Xuan","Dong",33);
+		Cliente C2=new Cliente ("Alcides","Quinteros",30);
+		Cuenta Cu1=new Cuenta(23,"Xuan",34,true);
+		Cuenta Cu2=new Cuenta(24,"Alcides",45,false);
+		
+		Operaciones op1 = new Operaciones ();
+		
+		op1.transferencias(Cu1, Cu2, 10);
+		
 	}
 
 }
