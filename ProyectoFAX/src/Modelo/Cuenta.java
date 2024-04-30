e2450fd91d175ae1510eed07c43c23057cdeb702
wugena <162334776+wugena@users.noreply.github.com>
2024-04-30 17:12:58 +0200

    crar paquete modelo con clase clientes y cuenta  -Xuan

--- ProyectoFAX/src/Modelo/Cuenta.java
+++ ProyectoFAX/src/Modelo/Cuenta.java
@@ -1,5 +1,19 @@
 package Modelo;
 
 public class Cuenta {
-
+	private int iban;
+	private String clientes;
+	private int cantidad;
+	private boolean estado ;
+	public Cuenta(int iban, String clientes, int cantidad, boolean estado) {
+		this.iban = iban;
+		this.clientes = clientes;
+		this.cantidad = cantidad;
+		this.estado = true;
+	}
+	
+	Public void transferencia () {
+		
+	}
+	
 }
